8ef68c2cfe0452692a8952cb82c1822af834f467
unknown <alansh2@illinois.edu>
2024-10-22 21:26:46 -0500

    added comments to decsg2.m

--- decsg2.m
+++ decsg2.m
@@ -24,13 +24,14 @@ for i = 2:numreg
     n2 = size(rgns{i},1);
     e1 = [1:n1;2:n1 1].';
     e2 = [1:n2;2:n2 1].';
+    % find intersections of boundaries
     [p,A] = lineline([pgon(e1(:,1),:) pgon(e1(:,2),:)],[rgns{i}(e2(:,1),:) rgns{i}(e2(:,2),:)]);
     if isempty(p), continue; end
 
     in1 = inpoly2(pgon,rgns{i},e2);
     in2 = inpoly2(rgns{i},pgon,e1);
 
-    idx = find(A) - 1;
+    idx = find(A) - 1; % linear indices
     l1 = [mod(idx,size(A,1))+1;0]; % row index with pad
     l2 = [floor(idx/size(A,1))+1;0]; % column index with pad
 
@@ -62,22 +63,25 @@ for i = 2:numreg
     pgon = [pgon;p;rgns{i}];
     edge = [e1;e2];
 
+    % sort vertices to form a simple polygon
+    % create matrix M where M(i,j) = 1 if vertices i and j are connected; 0 otherwise
+    M = sparse(edge,edge(:,[2 1]),true(numel(edge),1),size(pgon,1),size(pgon,1));
     ordering = zeros(size(edge,1),1);
-    B = sparse(edge,edge(:,[2 1]),true(numel(edge),1),size(pgon,1),size(pgon,1));
-    ordering(1) = edge(1,1);
-    skip = edge(1,2);
+    ordering(1) = edge(1,1); % start from any edge vertex
+    skip = edge(1,2); % prevent revisiting of previous nodes
     for j = 2:size(edge,1)
-        candidates = find(B(:,ordering(j-1)));
+        candidates = find(M(:,ordering(j-1)));
         ordering(j) = candidates(candidates~=skip);
         skip = ordering(j-1);
     end
 
-    pgon = pgon(ordering,:);
+    pgon = pgon(ordering,:); % sorted
 
     %EXAMPLE
     % t = 2*pi*(0:99).'/100;
-    % gd = [5;0.1;0.1;0.1;2;2;-2;0;2;2;-2;100;cos(t);sin(t)];
-    % ns = char('rect1','C1');
+    % gd = [5;0.1;0.1;0.1;2;2;-2;0;2;2;-2;100;cos(t);sin(t);3;0;-1;-2;0;2;0];
+    % ns = char('rect1','C1','T1');
     % ns = ns.';
     % pgon = decsg2(gd,ns);
+    % plot(pgon(:,1),pgon(:,2))
 end
